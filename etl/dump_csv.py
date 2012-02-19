42b82b39201a5cc029c5f7c0fa8713ca4a23d52a
Friedrich Lindenberg <friedrich@pudo.org>
2012-02-19 14:08:41 +0100

    [exploratory] export CSV of legal statusses per country.

---
+++ etl/dump_csv.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+import sqlaload as sl
+
+
+company_types = """SELECT "legalStatus", "contactCountry", COUNT(*) AS numHits FROM representative GROUP BY
+                   "legalStatus", "contactCountry" ORDER BY "legalStatus" ASC;"""
+
+def dump_query(engine, q, file_name):
+    fh = open(file_name, 'wb')
+    rp = engine.execute(q)
+    sl.dump_csv(sl.resultiter(rp), fh)
+
+if __name__ == '__main__':
+    import sys
+    assert len(sys.argv)==2, "Usage: %s [engine-url]"
+    engine = sl.connect(sys.argv[1])
+    dump_query(engine, company_types, "legalStatus.csv")
+
