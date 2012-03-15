98805f3243e789ca1607361b04fb12f1e793a1fa
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-16 00:03:26 +0100

    Update countries recon.

--- etl/countries.py
+++ etl/countries.py
@@ -1,8 +1,9 @@
 from urllib import urlopen 
 
-from recon.interactive import interactive, SQLALoadMemory
 from recon.local import CSVLocalEndpoint
 import sqlaload as sl
+
+from common import integrate_recon
 
 COUNTRIES_URL = 'countrycodes.csv'
 
@@ -20,19 +21,6 @@ def integrate_countries(engine):
                     'countryOfMember',
                     'countryOfMemberName', 'countryOfMemberCode',
                     memory_name='recon_countryNames')
-
-def integrate_recon(engine, table, qfunc, src_col, dst_name_col, dst_uri_col,
-        min_score=None, limit=200, memory_name=None):
-    if memory_name is None:
-        memory_name = "recon_%s_%s" % (table.name, src_col)
-    memory = SQLALoadMemory(engine, table=memory_name)
-    for row in sl.distinct(engine, table, src_col):
-        res = interactive(qfunc, row[src_col], min_score=min_score,
-                memory=memory, limit=limit)
-        if res is not None:
-            #print row.get(src_col), " -> ", res.name.encode('utf-8'), res.score
-            sl.upsert(engine, table, {src_col: row[src_col], dst_name_col: res.name, 
-                      dst_uri_col: res.uri}, [src_col])
 
 if __name__ == '__main__':
     import sys
