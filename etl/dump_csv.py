64a375d20b1986dcaee008efc6814adfcfaa9bce
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-16 00:01:48 +0100

    Support for dumping all entities.

--- etl/dump_csv.py
+++ etl/dump_csv.py
@@ -5,6 +5,7 @@ import sqlaload as sl
 
 company_types = """SELECT "legalStatus", "contactCountry", COUNT(*) AS numHits FROM representative GROUP BY
                    "legalStatus", "contactCountry" ORDER BY "legalStatus" ASC;"""
+entities = """SELECT * FROM entity;"""
 
 def dump_query(engine, q, file_name):
     fh = open(file_name, 'wb')
@@ -15,5 +16,6 @@ if __name__ == '__main__':
     import sys
     assert len(sys.argv)==2, "Usage: %s [engine-url]"
     engine = sl.connect(sys.argv[1])
-    dump_query(engine, company_types, "legalStatus.csv")
+    #dump_query(engine, company_types, "legalStatus.csv")
+    dump_query(engine, entities, "entity.csv")
 
