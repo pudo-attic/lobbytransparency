6b463fb3b72da4091778d5f67a229d7abb8bd361
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-16 16:02:31 +0100

    Mostly working: data loader for EUTR.

--- etl/load_grano.py
+++ etl/load_grano.py
@@ -6,68 +6,175 @@ import SETTINGS
 from schema import *
 
 
-def entity_upsert(session, collection, obj, *keys):
-    query = [k + ':' + obj[k] for k in keys]
-    res = session.get(session.api + '/' + collection,
-            params={'limit': 1, 'filter': query,
-                    'type': obj['type']})
-    matches = json.loads(res.text)
-    data = json.dumps(obj)
-    if matches:
-        res = session.put(session.api + '/' + collection + '/' + matches[0]['id'],
-            data=data)
-        assert res.ok, res.text
-        return json.loads(res.text)
-    else:
-        print session.api + '/' + collection
-        res = session.post(session.api + '/' + collection, data=data,
-            allow_redirects=True)
-        assert res.ok, res.text
-        return json.loads(res.text)
-
-
 def create_network(grano):
-    #rest_upsert(session, 'networks', NETWORK, 'slug')
     net = grano.getNetwork()
     if net:
         grano.updateNetwork(NETWORK)
     else:
         grano.createNetwork(NETWORK)
 
-    for type_, schema in (('entity', INTEREST),
-                          ('relation', TURNOVER)):
+    for type_, schema in (('entity', ACTOR),
+                          ('entity', ACTION_FIELD),
+                          ('entity', INTEREST),
+                          ('relation', TURNOVER),
+                          ('relation', MEMBERSHIP),
+                          ('relation', TOPIC),
+                          ('relation', EMPLOYMENT)):
         if grano.getSchema(type_, schema['name']):
             grano.updateSchema(type_, schema)
         else:
             grano.createSchema(type_, schema)
 
 
+def canonical_name(grano, engine, title, type=ACTOR['name']):
+    entity_table = sl.get_table(engine, 'entity')
+    res = sl.find_one(engine, entity_table, etlFingerPrint=title)
+    if res is None or not 'canonicalName' in res or \
+        res['canonicalName'] is None or title == res['canonicalName']:
+        return title
+    nonCanon = grano.findEntity(type, title=title)
+    if nonCanon:
+        grano.deleteEntity(nonCanon)
+    return res['canonicalName']
 
-def create_reps(session, engine):
+
+def get_financial_data(engine, rep):
+    fds = list(sl.find(engine, sl.get_table(engine, 'financialData'),
+        representativeEtlId=rep['etlId']))
+    fd = max(fds, key=lambda f: f.get('endDate'))
+    fo = {}
+    for key, value in fd.items():
+        if key in [u'totalBudget', u'turnoverMin', u'costAbsolute', u'publicFinancingNational',
+            u'otherSourcesDonation', u'eurSourcesProcurement', u'costMax', u'eurSourcesGrants',
+            u'otherSourcesContributions', u'publicFinancingTotal', u'turnoverAbsolute',
+            u'turnoverMax', u'costMin', u'directfdCostsMin', u'directfdCostsMax',
+            u'publicFinancingInfranational', u'otherSourcesTotal']:
+            if value is not None:
+                value = int(float(value))
+        key = 'fd' + key[0].upper() + key[1:]
+        fo[key] = value
+    return fo
+
+
+def load_persons(grano, engine, rep):
+    for person in sl.find(engine, sl.get_table(engine, 'person'),
+        representativeEtlId=rep['etlId']):
+        del person['id']
+        title = canonical_name(grano, engine, person['etlFingerPrint'])
+        psn = grano.findEntity(ACTOR['name'], title=title) or {}
+        psn['type'] = ACTOR['name']
+        psn['title'] = title
+        psn['firstName'] = person['firstName']
+        psn['lastName'] = person['lastName']
+        psn['salutation'] = person['title']
+        psn['actsAsPerson'] = True
+        psn = grano.updateEntity(psn)
+
+        rel = grano.findRelation(EMPLOYMENT['name'],
+                source_id=rep['id'],
+                target_id=psn['id']) or {}
+        rel['type'] = EMPLOYMENT['name']
+        rel['source'] = rep
+        rel['target'] = psn
+        rel['role'] = person['role']
+        rel = grano.updateRelation(rel)
+
+
+def load_organisations(grano, engine, rep):
+    for org in sl.find(engine, sl.get_table(engine, 'organisation'),
+        representativeEtlId=rep['etlId']):
+        del org['id']
+        title = canonical_name(grano, engine, org['name'])
+        ent = grano.findEntity(ACTOR['name'], title=title) or {}
+        ent['type'] = ACTOR['name']
+        ent['title'] = title
+        ent['members'] = int(float(org['numberOfMembers'] or 0))
+        ent['actsAsOrganisation'] = True
+        ent = grano.updateEntity(ent)
+
+        rel = grano.findRelation(MEMBERSHIP['name'],
+                source_id=rep['id'],
+                target_id=ent['id']) or {}
+        rel['type'] = MEMBERSHIP['name']
+        rel['source'] = rep
+        rel['target'] = ent
+        rel = grano.updateRelation(rel)
+
+
+def load_clients(grano, engine, rep):
+    for fdto in sl.find(engine, sl.get_table(engine, 'financialDataTurnover'),
+        representativeEtlId=rep['etlId']):
+        del fdto['id']
+        title = canonical_name(grano, engine, fdto['name'])
+        client = grano.findEntity(ACTOR['name'], title=title) or {}
+        client.update(fdto)
+        client['type'] = ACTOR['name']
+        client['title'] = title
+        client['actsAsClient'] = True
+        client = grano.updateEntity(client)
+
+        rel = grano.findRelation(TURNOVER['name'],
+                source_id=client['id'],
+                target_id=rep['id']) or {}
+        rel['type'] = TURNOVER['name']
+        rel['source'] = client
+        rel['target'] = rep
+        rel['min'] = int(float(fdto['min'] or 0))
+        rel['max'] = int(float(fdto['max'] or 0))
+        rel = grano.updateRelation(rel)
+
+PROPS = {}
+def load_proptable(grano, engine, rep, prop, schema):
+    for row in sl.find(engine, sl.get_table(engine, prop),
+        representativeEtlId=rep['etlId']):
+        if (prop, row[prop]) in PROPS:
+            int_ = PROPS[(prop, row[prop])]
+        else:
+            del row['id']
+            int_ = grano.findEntity(schema['name'], title=row[prop]) or {}
+            if int_.get('title') != row[prop]:
+                int_['type'] = schema['name']
+                int_['title'] = row[prop]
+                int_ = grano.updateEntity(int_)
+            PROPS[(prop, row[prop])] = int_
+
+        rel = grano.findRelation(TOPIC['name'],
+                source_id=rep['id'],
+                target_id=int_['id']) or {}
+        if not rel.get('id'):
+            rel['type'] = TOPIC['name']
+            rel['source'] = rep
+            rel['target'] = int_
+            grano.updateRelation(rel)
+
+
+def load_interests(grano, engine, rep):
+    return load_proptable(grano, engine, rep, 'interest', INTEREST)
+
+
+def load_action_fields(grano, engine, rep):
+    return load_proptable(grano, engine, rep, 'actionField', ACTION_FIELD)
+
+
+def load_representatives(grano, engine):
     for rep in sl.find(engine, sl.get_table(engine, 'representative')):
-        rep['type'] = 'interest'
-        rep['members'] = int(float(rep['members']))
-        rep['title'] = rep['originalName']
-        rep = convert_types(rep)
-        rep_ent = entity_upsert(session, NETWORK['slug'] + '/entities', rep, 'identificationCode')
-        for fdto in sl.find(engine, sl.get_table(engine, 'financialDataTurnover'), representativeEtlId=rep['etlId']):
-            client = {
-                'title': fdto['name'],
-                'etlId': rep['etlId'],
-                'etlFingerPrint': fdto['etlFingerPrint'],
-                'type': 'interest'
-                }
-            client_ent = entity_upsert(session, NETWORK['slug'] + '/entities', client, 'title')
-            rel = {
-                'source': rep_ent['id'],
-                'target': client_ent['id'],
-                'source_id': rep_ent['id'],
-                'target_id': client_ent['id'],
-                'min': int(float(fdto['min'] or 0)),
-                'max': int(float(fdto['max'] or 0)),
-                'type': RELATION_TURNOVER_SCHEMA['name']
-                }
-            client_ent = entity_upsert(session, NETWORK['slug'] + '/relations', rel, 'source_id', 'target_id')
+        del rep['id']
+        # TODO: name resolution
+        title = canonical_name(grano, engine, rep['originalName'])
+        rep_ent = grano.findEntity(ACTOR['name'], title=title) or {}
+        rep_ent.update(rep)
+        rep_ent['type'] = ACTOR['name']
+        rep_ent['actsAsRepresentative'] = True
+        rep_ent['members'] = int(float(rep['members']))
+        rep_ent['title'] = title
+        # TODO: other financial sources
+        rep_ent.update(get_financial_data(engine, rep_ent))
+        rep_ = grano.updateEntity(rep_ent)
+        load_clients(grano, engine, rep_)
+        load_organisations(grano, engine, rep_)
+        load_persons(grano, engine, rep_)
+        load_interests(grano, engine, rep_)
+        load_action_fields(grano, engine, rep_)
 
 
 if __name__ == '__main__':
@@ -78,4 +185,4 @@ if __name__ == '__main__':
         api_user=SETTINGS.GRANO_AUTH[0],
         api_password=SETTINGS.GRANO_AUTH[1])
     create_network(grano)
-    #create_reps(session, engine)
+    load_representatives(grano, engine)
--- etl/schema.py
+++ etl/schema.py
@@ -22,10 +22,87 @@ TURNOVER = {
         }
     }
 
-INTEREST = {
-    'label': 'Registered Interest',
-    'name': 'interest',
+EMPLOYMENT = {
+    'label': 'Employment',
+    'name': 'employment',
     'attributes': {
+        'role': {
+            'type': 'string',
+            'label': 'Role',
+            'help': ''
+            }
+        }
+    }
+
+MEMBERSHIP = {
+    'label': 'Membership',
+    'name': 'member',
+    'attributes': {}
+    }
+
+TOPIC = {
+    'label': 'Topic',
+    'name': 'topic',
+    'attributes': {}
+    }
+
+INTEREST = {
+    'label': 'Interest',
+    'name': 'interest',
+    'attributes': {}
+    }
+
+ACTION_FIELD = {
+    'label': 'Action Field',
+    'name': 'action_field',
+    'attributes': {}
+    }
+
+ACTOR = {
+    'label': 'Registered Interest',
+    'name': 'actor',
+    'attributes': {
+        'actsAsRepresentative': {
+            'type': 'boolean',
+            'label': 'Actor is a representative',
+            'missing': '0',
+            'help': ''
+            },
+        'actsAsClient': {
+            'type': 'boolean',
+            'label': 'Actor is a client',
+            'missing': '0',
+            'help': ''
+            },
+        'actsAsPerson': {
+            'type': 'boolean',
+            'label': 'Actor is a natual person',
+            'missing': '0',
+            'help': ''
+            },
+        'actsAsOrganisation': {
+            'type': 'boolean',
+            'label': 'Actor is an organisation',
+            'missing': '0',
+            'help': ''
+            },
+
+        'firstName': {
+            'type': 'string',
+            'label': 'First Name',
+            'help': ''
+            },
+        'lastName': {
+            'type': 'string',
+            'label': 'Last Name',
+            'help': ''
+            },
+        'salutation': {
+            'type': 'string',
+            'label': 'Salutation',
+            'help': ''
+            },
+
         'acronym': {
             'type': 'string',
             'label': 'Acronym',
@@ -154,6 +231,108 @@ INTEREST = {
         'webSiteURL': {
             'type': 'string',
             'label': 'Web site URL',
+            'help': ''
+            },
+
+
+        'fdDirectfdCostsMax': {
+            'type': 'integer',
+            'label': 'Direct costs (max)',
+            'help': ''
+            },
+        'fdType': {
+            'type': 'string',
+            'label': 'Financial reporting type',
+            'help': ''
+            },
+        'fdPublicFinancingTotal': {
+            'type': 'integer',
+            'label': 'Public financing (total)',
+            'help': ''
+            },
+        'fdOtherSourcesDonation': {
+            'type': 'integer',
+            'label': 'Other sources: Donations',
+            'help': ''
+            },
+        'fdTurnoverAbsolute': {
+            'type': 'integer',
+            'label': 'Turnover (absolute)',
+            'help': ''
+            },
+        'fdOtherSourcesTotal': {
+            'type': 'integer',
+            'label': 'Other sources (total)',
+            'help': ''
+            },
+        'fdEurSourcesGrants': {
+            'type': 'integer',
+            'label': 'European financing (grants)',
+            'help': ''
+            },
+        'fdCostMax': {
+            'type': 'integer',
+            'label': 'Cost (max)',
+            'help': ''
+            },
+        'fdPublicFinancingNational': {
+            'type': 'integer',
+            'label': 'Public financing (national)',
+            'help': ''
+            },
+        'fdDirectfdCostsMin': {
+            'type': 'integer',
+            'label': 'Direct costs (max)',
+            'help': ''
+            },
+        'fdTotalBudget': {
+            'type': 'integer',
+            'label': 'Total budget',
+            'help': ''
+            },
+        'fdStartDate': {
+            'type': 'date',
+            'label': 'Financial reporting start date',
+            'help': ''
+            },
+        'fdTurnoverMax': {
+            'type': 'integer',
+            'label': 'Turnover (max)',
+            'help': ''
+            },
+        'fdEurSourcesProcurement': {
+            'type': 'integer',
+            'label': 'European financing (procurement)',
+            'help': ''
+            },
+        'fdPublicFinancingInfranational': {
+            'type': 'integer',
+            'label': 'Public financing (infranational)',
+            'help': ''
+            },
+        'fdCostMin': {
+            'type': 'integer',
+            'label': 'Cost (min)',
+            'help': ''
+            },
+        'fdEndDate': {
+            'type': 'date',
+            'label': 'Financial reporting end date',
+            'help': ''
+            },
+        'fdTurnoverMin': {
+            'type': 'integer',
+            'label': 'Turnover (max)',
+            'help': ''
+            },
+        'fdCostAbsolute': {
+            'type': 'integer',
+            'label': 'Cost (absolute)',
+            'help': ''
+            },
+        'fdOtherSourcesContributions': {
+            'type': 'integer',
+            'label': 'Other sources (contributions)',
             'help': ''
             }
         }
