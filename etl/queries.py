9bd97868129c2a83ec8f55ebd47953a08ab040fd
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-24 13:39:02 +0100

    Additional reports as per google doc.

--- etl/queries.py
+++ etl/queries.py
@@ -6,14 +6,29 @@ QUERIES = [
         'query': 'SELECT * FROM entity_actor'
     },
     {
-        'name': 'companies_by_exp',
-        'label': 'TOP N companies (C II: companies and groups) spending most on lobbying',
-        'query': 'SELECT id, title, "contactCountry", "fdCostAbsolute" as expenditure FROM entity_actor WHERE "subCategoryId" = 21 ORDER BY "fdCostAbsolute" DESC NULLS LAST'
+        'name': 'actor_by_exp',
+        'label': 'Actors spending most on lobbying in a subcategory',
+        'query': 'SELECT id, title, "contactCountry", "fdCostAbsolute" as expenditure FROM entity_actor WHERE "subCategoryId" = :subCategoryId ORDER BY "fdCostAbsolute" DESC NULLS LAST'
     },
     {
         'name': 'rep_by_country',
         'label': 'Representatitives by country',
         'query': 'SELECT "contactCountry", COUNT(*) as number FROM entity_actor WHERE "actsAsRepresentative" = true GROUP BY "contactCountry" ORDER BY number DESC NULLS LAST'
+    },
+    {
+        'name': 'actor_by_turnover',
+        'label': 'Actors with the biggest lobbying turnover in a subcategory',
+        'query': 'SELECT id, title, "contactCountry", "fdTurnoverMax" as turnover FROM entity_actor WHERE "subCategoryId" = :subCategoryId ORDER BY "fdTurnoverMax" DESC NULLS LAST'
+    },
+    {
+        'name': 'actor_by_fte',
+        'label': 'Actors with the most lobbyists employed in a subcategory',
+        'query': 'SELECT ea.id AS id, ea.title AS title, ea."contactCountry" as "contactCountry", ea.members AS members, COUNT(re.id) as accreditations FROM entity_actor ea LEFT JOIN relation_employment re ON re.source_id = ea.id WHERE ea."subCategoryId" = :subCategoryId AND re.role = \'accredited\' GROUP BY ea.id, ea.title, ea.members, ea."contactCountry" ORDER BY members DESC NULLS LAST'
+    },
+    {
+        'name': 'fte_by_subcategory',
+        'label': 'Categories of actors with the most lobbyists employed',
+        'query': 'SELECT ea."subCategory" AS "subCategory", COUNT(ea.members) AS members, (SELECT COUNT(re.id) FROM relation_employment re LEFT JOIN entity_actor e ON re.source_id = e.id WHERE e."subCategory" = ea."subCategory" AND re.role = \'accredited\') as accreditations FROM entity_actor ea GROUP BY ea."subCategory" ORDER BY members DESC NULLS LAST'
     }
 
 ]
