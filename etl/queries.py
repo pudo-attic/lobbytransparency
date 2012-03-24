9364bed1d71a466d8c481f53bdcdaf612eceaa23
Friedrich Lindenberg <friedrich@pudo.org>
2012-03-24 11:10:38 +0100

    Begin to factor out reports.

--- etl/queries.py
+++ etl/queries.py
@@ -4,10 +4,16 @@ QUERIES = [
         'name': 'test',
         'label': 'Test Query',
         'query': 'SELECT * FROM entity_actor'
-        },
+    },
     {
-        'name': 's1',
+        'name': 'companies_by_exp',
         'label': 'TOP N companies (C II: companies and groups) spending most on lobbying',
-        'query': 'SELECT id, title, "contactCountry", "fdCostAbsolute" as expenditure FROM entity_actor WHERE "subCategoryId" = 21 ORDER BY "fdCostAbsolute" DESC'
+        'query': 'SELECT id, title, "contactCountry", "fdCostAbsolute" as expenditure FROM entity_actor WHERE "subCategoryId" = 21 ORDER BY "fdCostAbsolute" DESC NULLS LAST'
+    },
+    {
+        'name': 'rep_by_country',
+        'label': 'Representatitives by country',
+        'query': 'SELECT "contactCountry", COUNT(*) as number FROM entity_actor WHERE "actsAsRepresentative" = true GROUP BY "contactCountry" ORDER BY number DESC NULLS LAST'
     }
+
 ]
